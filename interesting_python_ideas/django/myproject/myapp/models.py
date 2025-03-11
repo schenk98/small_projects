585ba37861c8a71665b93004d24b5ec39f24dc0a
schenk98 <jakub.schenk1@gmail.com>
2025-03-11 14:37:56 +0100

    pexeso part2

--- interesting_python_ideas/django/myproject/myapp/models.py
+++ interesting_python_ideas/django/myproject/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class PexesoCard(models.Model):
+    czech_word = models.CharField(max_length=100)
+    english_word = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.czech_word} - {self.english_word}"
--- interesting_python_ideas/django/myproject/myproject/settings.py
+++ interesting_python_ideas/django/myproject/myproject/settings.py
@@ -74,7 +74,6 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
